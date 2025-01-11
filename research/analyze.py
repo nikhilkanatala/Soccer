155693a171357e5b3015063450ea70d279439b42
nikhilkanatala <kanatalanikhil25@gmail.com>
2025-01-10 23:29:03 -0500

    loading events data

--- research/analyze.py
+++ research/analyze.py
@@ -1,5 +1,14 @@
 import pandas  as pd
+import os
 
 # Load the data - path 'data/competitions.json'
-data = pd.read_json('/Users/nikhilkanatala/Desktop/open-data/competitions.json')
+# Navigate the folder '/Users/nikhilkanatala/Desktop/open-data/events' and load all the jsons in there
+
+dir_path = '/Users/nikhilkanatala/Desktop/open-data/data/events/'
+data = pd.DataFrame()
+for file in os.listdir(dir_path):
+    path = os.path.join(dir_path, file)
+    if os.path.isfile(path):
+        data = pd.concat([data, pd.read_json(path)])
+
 data.head()
